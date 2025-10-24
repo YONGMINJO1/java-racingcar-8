88543589b9b3a92f8279f8342e6660a22614c6e8
YONGMINJO1 <iy0594@naver.com>
2025-10-24 12:23:38 +0900

    feat: RandomGenerator 기능 구현

--- src/main/java/racingcar/utils/RandomGenerator.java
+++ src/main/java/racingcar/utils/RandomGenerator.java
@@ -1,4 +1,15 @@
 package racingcar.utils;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class RandomGenerator {
+
+    private static final int MIN_RANDOM = 0;
+    private static final int MAX_RANDOM = 9;
+
+    private RandomGenerator() { }
+
+    public static int generate() {
+        return Randoms.pickNumberInRange(MIN_RANDOM, MAX_RANDOM);
+    }
 }
