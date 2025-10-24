5c987bfe6ce67794de47c2174d5728a140e9c5c9
YONGMINJO1 <iy0594@naver.com>
2025-10-24 15:50:50 +0900

    feat: GameController (게임진행) 기능 구현

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -1,4 +1,39 @@
 package racingcar.controller;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import racingcar.domain.Car;
+import racingcar.domain.RacingGame;
+import racingcar.view.OutputView;
+
 public class GameController {
+
+    private final RacingGame game;
+    private final List<List<Integer>> rounds;
+
+    public GameController(RacingGame game, List<List<Integer>> rounds) {
+        this.game = game;
+        this.rounds = rounds;
+    }
+
+    public void play() {
+        for (List<Integer> randomNumbers : rounds) {
+            game.moveCars(randomNumbers);
+            OutputView.printCarPositions(game.getCars());
+        }
+
+        List<String> winners = game.getCars().stream()
+                .filter(car -> car.getPosition() == getMaxPosition())
+                .map(Car::getName)
+                .collect(Collectors.toList());
+
+        OutputView.printWinners(winners);
+    }
+
+    private int getMaxPosition() {
+        return game.getCars().stream()
+                .mapToInt(Car::getPosition)
+                .max().
+                orElse(0);
+    }
 }
