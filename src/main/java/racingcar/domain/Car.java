38c1ec4a471b5ebe1ec5a965e704f7a708648ddb
YONGMINJO1 <iy0594@naver.com>
2025-10-24 11:27:42 +0900

    feat: 자동차 이동 기능 구현

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -22,4 +22,8 @@ public class Car {
     public int getPosition() {
         return position;
     }
+
+    public void move() {
+        position++;
+    }
 }
--- src/main/java/racingcar/domain/RacingGame.java
+++ src/main/java/racingcar/domain/RacingGame.java
@@ -1,4 +1,23 @@
 package racingcar.domain;
 
+import java.util.List;
+
 public class RacingGame {
+    private final List<Car> cars;
+
+    public RacingGame(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    public void moveCars(List<Integer> randomNumbers) {
+        for (int i = 0; i < cars.size(); i++) {
+            if (randomNumbers.get(i) >= 4) {
+                cars.get(i).move();
+            }
+        }
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
 }
