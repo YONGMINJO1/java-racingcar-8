1de520884467665f4345be86c185c8dfdc491f41
YONGMINJO1 <iy0594@naver.com>
2025-10-24 14:36:13 +0900

    feat: OutputView (자동차 위치, 최종 우승자 출력) 기능 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,4 +1,26 @@
 package racingcar.view;
 
+import java.util.List;
+import java.util.Map;
+
 public class OutputView {
+
+    public static void printCarPositions(Map<String, Integer> carPositions) {
+        for (String name : carPositions.keySet()) {
+            int position = carPositions.get(name);
+            System.out.print(name + " : ");
+            for (int i = 0; i < position; i++) {
+                System.out.print("-");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static void printWinners(List<String> winners) {
+
+        String winnersName = String.join(", ", winners);
+        System.out.println("최종 우승자: " + winnersName);
+
+    }
 }
