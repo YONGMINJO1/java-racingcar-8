f99cf9e921b1a8e16d8634fc9b345222aaa1158d
YONGMINJO1 <iy0594@naver.com>
2025-10-24 16:07:29 +0900

    feat: 자동차 경주게임 전체기능 구현 및 통합테스트 통과

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,37 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.List;
+import racingcar.controller.GameController;
+import racingcar.domain.Car;
+import racingcar.domain.RacingGame;
+import racingcar.utils.RandomGenerator;
+import racingcar.view.InputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        List<String> carNames = InputView.carNames();
+        int rounds = InputView.tryCount();
+
+        List<Car> cars = new ArrayList<>();
+        for (String name : carNames) {
+            cars.add(new Car(name));
+        }
+
+        RacingGame game = new RacingGame(cars);
+
+        List<List<Integer>> allrounds = new ArrayList<>();
+        for (int i = 0; i < rounds; i++) {
+            List<Integer> roundNumbers = new ArrayList<>();
+            for (int j = 0; j < cars.size(); j++) {
+                roundNumbers.add(RandomGenerator.generate());
+            }
+            allrounds.add(roundNumbers);
+        }
+
+        GameController controller = new GameController(game, allrounds);
+        controller.play();
+
     }
 }
