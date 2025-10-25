9ebcd35bbe67ce94cff6b082f02c7fb5e59d1bfe
YONGMINJO1 <iy0594@naver.com>
2025-10-25 20:32:48 +0900

    refactor: InputView 메서드에서 valiator 분리

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -3,17 +3,39 @@ package racingcar.view;
 import camp.nextstep.edu.missionutils.Console;
 import java.util.Arrays;
 import java.util.List;
+import racingcar.validator.InputValidator;
 
 public class InputView {
 
+    private static String getUserInput(String message) {
+        System.out.println(message);
+        return Console.readLine();
+    }
+
     public static List<String> carNames() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String input = Console.readLine();
-        return Arrays.asList(input.split(","));
+        String input = readCarNamesInput();
+        List<String> names = parseCarNames(input);
+        InputValidator.validateCarNames(names);
+        return names;
+    }
+
+    private static String readCarNamesInput() {
+        return getUserInput("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+
+    public static List<String> parseCarNames(String input) {
+        return Arrays.stream(input.split(","))
+                .map(String::trim)
+                .toList();
     }
 
     public static int tryCount() {
-        System.out.println("시도할 횟수는 몇 회인가요?");
-        return Integer.parseInt(Console.readLine());
+        String input = readTryCountInput();
+        InputValidator.validateTryCount(input);
+        return Integer.parseInt(input);
+    }
+
+    private static String readTryCountInput() {
+        return getUserInput("시도할 횟수는 몇 회인가요?");
     }
 }
